896d2e7a360fa02c9653609c7aaa86a0cfbabe50
Vivekanand Pujar <vivek@anviksha.io>
2024-11-14 17:27:06 +0530

    done

--- apps/admin/src/app/app.component.ts
+++ apps/admin/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { ButtonComponent } from 'libs/button/src/lib/button/button.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, ButtonComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
